55297b4b074f5752475c1b9e8e9e93f240f097fd
hyunbinHwang <hello.git.kj04419@naver.com>
2023-03-03 10:05:39 +0900

    first commit

--- src/chap_02/_Quiz_02.java
+++ src/chap_02/_Quiz_02.java
@@ -3,7 +3,7 @@ package chap_02;
 // 조건1. 키가 120cm 이상인 경우에만 탑승 가능
 // 조건2. 삼항 연산자 이용
 // 실행 결과 : 키가 115cm 이므로 탑승 불가능합니다
-// 실행 결과 : 키가 121cm 이므로 탑승 가능합니다
+// 실행 결과 : 키가 121cm 이므로 탑승 가능합니다.
 public class _Quiz_02 {
     public static void main(String[] args) {
         int height = 121;
