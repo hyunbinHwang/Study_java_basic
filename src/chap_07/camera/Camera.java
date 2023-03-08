78046f788a3011115e79451e6c7444b1ed181cea
hyunbinHwang <kj004419@naver.com>
2023-03-08 18:36:23 +0900

    20230308

--- src/chap_07/camera/Camera.java
+++ src/chap_07/camera/Camera.java
@@ -4,7 +4,12 @@ public class Camera { // 부모 클래스
     public String name;
 
     public Camera() {
-        this.name = "카메라";
+        this("카메라");
+    }
+
+    protected Camera(String name) {
+        this.name = name;
+
     }
 
     public void takePicture() {
@@ -15,5 +20,9 @@ public class Camera { // 부모 클래스
     public void recordVideo() {
         // 동영상 녹화
         System.out.println(this.name + " : 동영상을 녹화합니다.");
+    }
+
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 사진 촬영, 동영상 녹화");
     }
 }
